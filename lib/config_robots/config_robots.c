ca40e118cc503471c5b73efe7d00e3198d73c07e
Paul <vitalium.02@gmail.com>
2025-04-15 21:54:56 +0200

    add configuration for all the pamis

--- lib/config_robots/config_robots.c
+++ lib/config_robots/config_robots.c
@@ -17,3 +17,52 @@ bool avoidance[] = {false,false,false,false,true};
 uint32_t numPoints = 2;
 
 # endif
+
+#ifdef PAMI_2 
+
+// waypoints and avoidance need to have the same length
+Point waypoints[] = {
+    {1300, 1900},
+    {1300, 1800},
+    {1200,200},
+    {900,200},
+    {1200,400}
+};
+
+bool avoidance[] = {false,false,false,false,true};
+uint32_t numPoints = 2;
+
+# endif
+
+#ifdef PAMI_3
+
+// waypoints and avoidance need to have the same length
+Point waypoints[] = {
+    {1300, 1900},
+    {1300, 1800},
+    {1200,200},
+    {900,200},
+    {1200,400}
+};
+
+bool avoidance[] = {false,false,false,false,true};
+uint32_t numPoints = 2;
+
+# endif
+
+#ifdef PAMI_4
+
+// waypoints and avoidance need to have the same length
+Point waypoints[] = {
+    {1300, 1900},
+    {1300, 1800},
+    {1200,200},
+    {900,200},
+    {1200,400}
+};
+
+bool avoidance[] = {false,false,false,false,true};
+uint32_t numPoints = 2;
+
+# endif
+
--- lib/config_robots/config_robots.hh
+++ lib/config_robots/config_robots.hh
@@ -5,7 +5,7 @@
 
 # define TEST_MODE
 //# define PRINT_DISTANCES
-# define EVITEMENT 
+//# define EVITEMENT 
 
 # ifdef TEST_MODE
 # define GLOBAL_WAIT 2000
@@ -33,8 +33,8 @@ const uint8_t bouton_equipe = 10; // D10 low = blue vers le haut, hight= jaune (
 
 ////// chose which pami you want to use /////////////
 
-# define PAMI_1 // la superstar
-//# define PAMI_2
+//# define PAMI_1 // la superstar
+# define PAMI_2
 //# define PAMI_3
 //# define PAMI_4
 
@@ -74,73 +74,67 @@ const int SUPERSTAR_FLOOR_THRESHOLD = 75; //The distance above which we consider
 
 # endif
 
+#ifdef PAMI_2 
 
-#ifdef PAMI_2
+#define X_POSITION_START_ 70.0
+#define Y_POSITION_START_  1900
+#define TETA_POSITION_START_ 0.0
 
-float x_goal = 2500; // 1=2500 2=500 3=2400  4=500
-float y_goal = 1220; // 1=1400 2=1600 3=1000 4=600
-volatile bool go_to_1 = true ;
-float x_goal_2 = 2970;// 1=3000 2=200 3=2800 4=0
-float y_goal_2 = 1280; // 1=1400 2=1800 3=1000 4=600
-int avancement_depart = 1100 ; //1=1100 2=200 3=600 4=100
-int time_start = 1000; //1 = 90000s 2 = 90500s 3 = 90000s 4 = 90500s
-int depart = 0 ;
+#define MOTOR_RPM_ 100
+#define MOTOR_ACCEL_ 1400
+#define MOTOR_DECEL_ 300
+//#define MOTOR_DECEL_AVOIDEMENT 5000 // freinage brust s'il y a un obstacle
+//#define MOTOR_DECEL_FINISH_ 500
 
+#define MOTOR_ACCEL_DECEL_ROTATE_ 700 // 700
+
+#define COEF_ROTATE 493 // 1=493 2 =498 3=495 4=487
+#define COEF_STRAIGHT 1.0
+
+#define ADD_DELAY_START 0.0 // after the 85sec we add a extra delay to avoid all the pami to start at the same time. 
+const int SUPERSTAR_FLOOR_THRESHOLD = 75; //The distance above which we consider there is no floor  // True when the superstar is doing the superstar-specific movements, used to change how avoidances are triggered
 # endif
 
 #ifdef PAMI_3
 
-float x_goal = 550; // 1=2500 2=500 3=2400  4=500
-float y_goal = 1350; // 1=1400 2=1600 3=1000 4=600
-volatile bool go_to_1 = true ;
-float x_goal_2 = 150;// 1=3000 2=200 3=2800 4=0
-float y_goal_2 = 1650; // 1=1400 2=1800 3=1000 4=600
-int avancement_depart = 700; //1=1100 2=200 3=600 4=100
-int time_start = 0; //1 = 90000s 2 = 90500s 3 = 90000s 4 = 90500s
-int depart = 0 ;
+#define X_POSITION_START_ 70.0
+#define Y_POSITION_START_  1900
+#define TETA_POSITION_START_ 0.0
 
+#define MOTOR_RPM_ 100
+#define MOTOR_ACCEL_ 1400
+#define MOTOR_DECEL_ 300
+//#define MOTOR_DECEL_AVOIDEMENT 5000 // freinage brust s'il y a un obstacle
+//#define MOTOR_DECEL_FINISH_ 500
+
+#define MOTOR_ACCEL_DECEL_ROTATE_ 700 // 700
+
+#define COEF_ROTATE 493 // 1=493 2 =498 3=495 4=487
+#define COEF_STRAIGHT 1.0
+
+#define ADD_DELAY_START 0.0 // after the 85sec we add a extra delay to avoid all the pami to start at the same time. 
+const int SUPERSTAR_FLOOR_THRESHOLD = 75; //The distance above which we consider there is no floor  // True when the superstar is doing the superstar-specific movements, used to change how avoidances are triggered
 # endif
-
-/* ORIGINAL CODE USED FOR CUP
-#ifdef PAMI_4
-
-float x_goal = 500; // 1=2500 2=500 3=2400  4=500
-float y_goal = 420; // 1=1400 2=1600 3=1000 4=600
-volatile bool go_to_1 = true ;
-float x_goal_2 = 30;// 1=3000 2=200 3=2800 4=0
-float y_goal_2 = 480; // 1=1400 2=1800 3=1000 4=600
-int avancement_depart = 300 ; //1=1100 2=200 3=600 4=100
-int time_start = 3000; //1 = 90000s 2 = 90500s 3 = 90000s 4 = 90500s
-int depart = 0 ;
-
-# endif
-*/
-/*
-#ifdef PAMI_4
-
-float x_goal = 1000; // 1=2500 2=500 3=2400  4=500
-float y_goal = 0; // 1=1400 2=1600 3=1000 4=600
-volatile bool go_to_1 = true ;
-float x_goal_2 = 1000;// 1=3000 2=200 3=2800 4=0
-float y_goal_2 = 100; // 1=1400 2=1800 3=1000 4=600
-int avancement_depart = 0 ; //1=1100 2=200 3=600 4=100
-int time_start = 0; //1 = 90000s 2 = 90500s 3 = 90000s 4 = 90500s
-int depart = 0 ;
-
-# endif
-*/
 
 #ifdef PAMI_4
 
-float x_goal = 1300; // 1=2500 2=500 3=2400  4=500
-float y_goal = 0; // 1=1400 2=1600 3=1000 4=600
-volatile bool go_to_1 = true ;
-float x_goal_2 = 1300;// 1=3000 2=200 3=2800 4=0
-float y_goal_2 = 50; // 1=1400 2=1800 3=1000 4=600
-int avancement_depart = 0 ; //1=1100 2=200 3=600 4=100
-int time_start = 0; //1 = 90000s 2 = 90500s 3 = 90000s 4 = 90500s
-int depart = 0 ;
+#define X_POSITION_START_ 70.0
+#define Y_POSITION_START_  1900
+#define TETA_POSITION_START_ 0.0
 
+#define MOTOR_RPM_ 100
+#define MOTOR_ACCEL_ 1400
+#define MOTOR_DECEL_ 300
+//#define MOTOR_DECEL_AVOIDEMENT 5000 // freinage brust s'il y a un obstacle
+//#define MOTOR_DECEL_FINISH_ 500
+
+#define MOTOR_ACCEL_DECEL_ROTATE_ 700 // 700
+
+#define COEF_ROTATE 493 // 1=493 2 =498 3=495 4=487
+#define COEF_STRAIGHT 1.0
+
+#define ADD_DELAY_START 0.0 // after the 85sec we add a extra delay to avoid all the pami to start at the same time. 
+const int SUPERSTAR_FLOOR_THRESHOLD = 75; //The distance above which we consider there is no floor  // True when the superstar is doing the superstar-specific movements, used to change how avoidances are triggered
 # endif
 
 # endif
